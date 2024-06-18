45c71e73d492fae1edc5b183fc3fce804c9969a3
Tomas Dumont <jtdumont34@gmail.com>
2024-06-18 14:30:05 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+
+function solicitarNombre() {
+    let nombre = (prompt("Ingrese su nombre"));
+    alert("¡Bienvenido " + nombre + " a su conversor de monedas online!");
+}
+//Invocar función
+solicitarNombre();
